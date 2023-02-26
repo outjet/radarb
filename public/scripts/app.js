b70db47094977158ec3d7a9555513e6656dbe17b
outjet <git@outjet.com>
2023-02-26 14:35:31 -0500

    Sensors and cameras based on geolocation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,22 +1,123 @@
+function haversine(lat1, lon1, lat2, lon2) {
+  const R = 6371; // Earth's radius in km
+  const dLat = toRadians(lat2 - lat1);
+  const dLon = toRadians(lon2 - lon1);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c;
+  return d;
+}
+
+function toRadians(degrees) {
+  return degrees * Math.PI / 180;
+}
+
+window.addEventListener("load", () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      const lat = position.coords.latitude;
+      const lng = position.coords.longitude;
+      const boxsize = 8;
+      const latne = lat + boxsize/69;
+      const lngne = lng + boxsize/53;
+      const latsw = lat - boxsize/69;
+      const lngsw = lng - boxsize/53;
+
+      window.dispatchEvent(new CustomEvent("loadCameraData", {
+        detail: { latne, lngne, latsw, lngsw, lat, lng }
+      }));
+
+      window.dispatchEvent(new CustomEvent("loadSensorData", {
+        detail: { latne, lngne, latsw, lngsw, lat, lng }
+      }));
+    });
+  }
+});
+
+// CAMERAS 
+
+const displayCameraData = (data, userLat, userLng) => {
+  const cameraDistances = [];
+
+  // Calculate the distance from the user's location to each camera
+  data.results.forEach(camera => {
+    const distance = haversine(userLat, userLng, camera.latitude, camera.longitude);
+    cameraDistances.push({ camera, distance });
+  });
+
+  // Sort the cameras by their distance
+  cameraDistances.sort((a, b) => a.distance - b.distance);
+
+  // Display the closest four cameras
+  const imageGrid = document.querySelector(".image-grid");
+  imageGrid.innerHTML = ""; // clear previous cameras
+  for (let i = 0; i < Math.min(4, cameraDistances.length); i++) {
+    const camera = cameraDistances[i].camera;
+    const div = document.createElement("div");
+    const img = document.createElement("img");
+    img.setAttribute("src", camera.cameraViews[0].smallUrl);
+    img.setAttribute("alt", camera.description);
+    const caption = document.createElement("div");
+    caption.classList.add("caption");
+    if (camera.cameraViews[0].mainRoute.includes("Hilliard")) {
+      caption.textContent = 'I-90 between Hilliard/Mckinley';
+    } else {
+      caption.textContent = camera.cameraViews[0].mainRoute;
+    }
+    div.appendChild(img);
+    div.appendChild(caption);
+    imageGrid.appendChild(div);
+  }
+};
+
+window.addEventListener("loadCameraData", (event) => {
+  const { latne, lngne, latsw, lngsw, lat, lng } = event.detail;
+  fetch(`https://us-central1-radarb.cloudfunctions.net/getCameraData?latne=${latne}&lngne=${lngne}&latsw=${latsw}&lngsw=${lngsw}`)
+  // fetch(`http://127.0.0.1:5001/radarb/us-central1/getCameraData?latne=${latne}&lngne=${lngne}&latsw=${latsw}&lngsw=${lngsw}`)
+    .then(response => response.json())
+    .then(data => {
+      displayCameraData(data, lat, lng);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
+
+
 // SENSORS
-const displaySensorData = (data) => {
-  let counter = 0;
-  const sensorContainer = document.querySelector(".sensor-container");
+const displaySensorData = (data, lat, lng) => {
+  const sensorDistances = [];
+
+  // Calculate the distance from the user's location to each sensor
   data.results.forEach(result => {
     result.surfaceSensors.forEach(sensor => {
-      if (counter >= 3) return;
-      const div = document.createElement("div");
-      div.classList.add("sensor-box");
-      if (sensor.status === "Ice Watch") {
-        div.classList.add("IceWatch");
-      }
-      div.innerHTML = sensor.name.substring(0, sensor.name.length - 4);
-      div.innerHTML += "<br>" + "Status: " + sensor.status;
-      sensorContainer.appendChild(div);
-      counter++;
+      if (sensor.surfaceTemperature === -9999999.0) return;
+      const distance = haversine(lat, lng, sensor.latitude, sensor.longitude);
+      sensorDistances.push({ sensor, distance });
     });
   });
-  let refreshIntervalId;
+
+  // Sort the sensors by their distance
+  sensorDistances.sort((a, b) => a.distance - b.distance);
+
+  // Display the closest three sensors
+  const sensorContainer = document.querySelector(".sensor-container");
+  for (let i = 0; i < Math.min(3, sensorDistances.length); i++) {
+    const sensor = sensorDistances[i].sensor;
+    const div = document.createElement("div");
+    div.classList.add("sensor-box");
+    if (sensor.status === "Ice Watch") {
+      div.classList.add("IceWatch");
+    }
+    div.innerHTML = sensor.name.substring(0, sensor.name.length - 4);
+    div.innerHTML += "<br>" + "Status: " + sensor.status;
+    div.innerHTML += "<br>" + "Surface temp: " + sensor.surfaceTemperature;
+    sensorContainer.appendChild(div);
+  }
+
   const forecastDiv = document.createElement("div");
   forecastDiv.classList.add("sensor-box");
   forecastDiv.style.backgroundColor = "#DDD";
@@ -31,14 +132,6 @@ const displaySensorData = (data) => {
   clocksDiv.innerHTML = "<div><span id='local-time'></span> ET</div><div><span id='utc-time'></span> UTC</div><div><span id='refresh-paused' style='display:none;'>REFRESH PAUSED</span></div>";
   sensorContainer.appendChild(clocksDiv);
 
-  setTimeout(() => {
-    clearInterval(refreshIntervalId);
-    let clocks = document.getElementById("clocks");
-    clocks.style.backgroundColor = "crimson";
-    clocks.style.color = "white";
-    document.getElementById("refresh-paused").style.display = "block";
-  }, 180000);
-
   document.getElementById("clocks").addEventListener("click", function () {
     let clocks = document.getElementById("clocks");
     clocks.style.backgroundColor = "#DDD";
@@ -52,26 +145,24 @@ const displaySensorData = (data) => {
       });
     }, 4250);
   });
-
 };
 
-window.addEventListener("load", () => {
-  window.dispatchEvent(new Event("loadSensorData"));
-});
 
-window.addEventListener("loadSensorData", () => {
-  fetch("https://us-central1-radarb.cloudfunctions.net/getSensorData")
-    .then(response => response.json())
-    .then(data => {
-      displaySensorData(data);
-      updateTime();
-      setInterval(updateTime, 1000);
-    })
-    .catch(error => {
-      console.error(error);
-    });
-});
-
+  window.addEventListener("loadSensorData", (event) => {
+    const { latne, lngne, latsw, lngsw, lat, lng } = event.detail;
+   fetch(`https://us-central1-radarb.cloudfunctions.net/getSensorData?latne=${latne}&lngne=${lngne}&latsw=${latsw}&lngsw=${lngsw}`)
+    // fetch(`http://127.0.0.1:5001/radarb/us-central1/getSensorData?latne=${latne}&lngne=${lngne}&latsw=${latsw}&lngsw=${lngsw}`)
+      .then(response => response.json())
+      .then(data => {
+        displaySensorData(data, lat, lng);
+        updateTime();
+        setInterval(updateTime, 1000);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  });
+  
 window.addEventListener("load", () => {
   window.dispatchEvent(new Event("updateTime"));
 });
@@ -113,6 +204,8 @@ function updateTime() {
     document.getElementById("utc-time").innerHTML = UTCtime;
   }, 1000);
 }
+
+
 // WEATHER FORECAST
 window.addEventListener('load', function () {
   getWeatherForecast();
@@ -171,11 +264,11 @@ async function getWeatherForecast() {
         }
         const alertDiv = document.createElement("div");
         alertDiv.classList.add("weather-alert");
-        alertDiv.innerHTML = `Weather Alert: ${ alert.event } - ${ alert.description }`;
+        alertDiv.innerHTML = `Weather Alert: ${alert.event} - ${alert.description}`;
         crosswindContainer.appendChild(alertDiv);
       });
     }
-    
+
     const forecastContainer = document.querySelector('.forecast-container');
     forecastContainer.innerHTML = '';
 
@@ -201,49 +294,4 @@ async function getWeatherForecast() {
   }
 }
 
-
-
-// CAMERAS 
-
-const displayCameraData = (data) => {
-  const imageGrid = document.querySelector(".image-grid");
-  data.results.forEach(camera => {
-    const div = document.createElement("div");
-    const img = document.createElement("img");
-    img.setAttribute("src", camera.cameraViews[0].smallUrl);
-    img.setAttribute("alt", camera.description);
-    const caption = document.createElement("div");
-    caption.classList.add("caption");
-    caption.textContent = camera.cameraViews[0].mainRoute;
-    div.appendChild(img);
-    div.appendChild(caption);
-    imageGrid.appendChild(div);
-  });
-};
-
-window.addEventListener("load", () => {
-  window.dispatchEvent(new Event("loadCameraData"));
-});
-
-window.addEventListener("loadCameraData", () => {
-  fetch("https://us-central1-radarb.cloudfunctions.net/getCameraData")
-    .then(response => response.json())
-    .then(data => {
-      displayCameraData(data);
-    })
-    .catch(error => {
-      console.error(error);
-    });
-});
-
-window.addEventListener("load", () => {
-  let refreshIntervalId = setInterval(() => {
-    let images = document.querySelectorAll(".image-grid img");
-    images.forEach(img => {
-      let currentSrc = img.getAttribute("src");
-      img.setAttribute("src", currentSrc + "?t=" + new Date().getTime());
-    });
-  }, 4250);
-
-});
 
